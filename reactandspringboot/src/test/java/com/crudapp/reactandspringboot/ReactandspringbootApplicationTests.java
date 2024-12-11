1979baeedc175c5fce1956b935c3c7aec0db2dc6
brandon-le-1 <71300565+brandon-le-1@users.noreply.github.com>
2024-12-11 16:53:48 -0700

    Project initialized

---
+++ reactandspringboot/src/test/java/com/crudapp/reactandspringboot/ReactandspringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crudapp.reactandspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactandspringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
