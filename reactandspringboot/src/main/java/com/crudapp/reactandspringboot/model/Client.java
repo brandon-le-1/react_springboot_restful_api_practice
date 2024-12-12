203cfb4933ae36e9e5fca5359de48890ce5d4571
brandon-le-1 <71300565+brandon-le-1@users.noreply.github.com>
2024-12-11 17:08:50 -0700

    'client' model added

---
+++ reactandspringboot/src/main/java/com/crudapp/reactandspringboot/model/Client.java
@@ -0,0 +1,47 @@
+package com.crudapp.reactandspringboot.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
+@Table(name = "client")
+public class Client {
+    @Id
+    @GeneratedValue
+
+    private Long id;
+    private String name;
+    private String email;
+
+    public Client(Long id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
