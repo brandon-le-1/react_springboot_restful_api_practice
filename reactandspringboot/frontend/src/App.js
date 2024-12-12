029326b5562bf5eb3f91bda7b422e67081e5e7ba
brandon-le-1 <71300565+brandon-le-1@users.noreply.github.com>
2024-12-12 16:43:35 -0700

    front end retrives data from api and displays clients in frontend

--- reactandspringboot/frontend/src/App.js
+++ reactandspringboot/frontend/src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import logo from './logo.svg';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    clients: []
+  };
+
+  async componentDidMount() {
+    const response = await fetch('/clients');
+    const body = await response.json();
+    this.setState({clients: body});
+  }
+
+  render() {
+    const {clients} = this.state;
+    return (
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <div className="App-intro">
+              <h2>Clients</h2>
+              {clients.map(client =>
+                  <div key={client.id}>
+                    {client.name} ({client.email})
+                  </div>
+              )}
+            </div>
+          </header>
+        </div>
+    );
+  }
 }
-
 export default App;
+
